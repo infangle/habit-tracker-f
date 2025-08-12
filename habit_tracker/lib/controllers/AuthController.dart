12e81cc7404eef4eb8b03d4cbe45a0ab8e314135
infangle <lakewyetnayet93@gmail.com>
2025-08-12 14:08:19 +0300

    use mock api for auth

--- habit_tracker/lib/controllers/AuthController.dart
+++ habit_tracker/lib/controllers/AuthController.dart
@@ -1,6 +1,11 @@
 import 'package:get/get.dart';
+import 'package:habit_tracker/data/user_repository_impl.dart'; // Import the UserRepository implementation from the data layer
 
 class AuthController extends GetxController {
+  final UserRepositoryImpl
+  userRepository; // Change UserRepository to UserRepositoryImpl
+  AuthController(this.userRepository);
+
   RxString email = ''.obs;
   RxString password = ''.obs;
   RxString username = ''.obs;
@@ -18,18 +23,45 @@ class AuthController extends GetxController {
   }
 
   void loginUser() {
-    // Implement your login logic here using email and password
-    print(
-      'Logging in with email: ${email.value} and password: ${password.value}',
-    );
-    // Add your authentication logic
+    userRepository
+        .login(
+          email.value,
+          password.value,
+        ) // Use the email and password values from the RxStrings
+        .then((success) {
+          if (success) {
+            // Handle successful login, navigate to the next screen, etc.
+            print('Login successful!');
+          } else {
+            // Handle login failure, show error message, etc.
+            print('Login failed. Please try again.');
+          }
+        })
+        .catchError((error) {
+          // Handle any errors that occur during the login process
+          print('An error occurred: $error');
+        });
   }
 
   void signUpUser() {
-    // Implement your signup logic here using email, password, and username
-    print(
-      'Signing up with email: ${email.value}, password: ${password.value}, and username: ${username.value}',
-    );
-    // Add your authentication logic
+    userRepository
+        .signup(
+          email.value,
+          password.value,
+          username.value,
+        ) // Use the email, password, and username values from the RxStrings
+        .then((success) {
+          if (success) {
+            // Handle successful signup, navigate to the next screen, etc.
+            print('Signup successful!');
+          } else {
+            // Handle signup failure, show error message, etc.
+            print('Signup failed. Please try again.');
+          }
+        })
+        .catchError((error) {
+          // Handle any errors that occur during the signup process
+          print('An error occurred: $error');
+        });
   }
 }
